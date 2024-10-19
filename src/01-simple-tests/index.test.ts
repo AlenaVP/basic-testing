c4d8a7f2d8e1c3934fb44ac5c4d4f326a68132af
AlenaVP Legion <poprugo@gmail.com>
2024-10-19 21:52:13 +0200

    feat: implement simple-tests

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,32 +1,47 @@
-// Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({a: 3, b: 5, action: '+'});
+    expect(result).toBe(8);
   });
 
   test('should subtract two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({a: 3, b: 5, action: '-'});
+    expect(result).toBe(-2);
   });
 
   test('should multiply two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({a: 3, b: 5, action: '*'});
+    expect(result).toBe(15);
   });
 
   test('should divide two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({a: 3, b: 5, action: '/'});
+    expect(result).toBe(0.6);
   });
 
   test('should exponentiate two numbers', () => {
-    // Write your test here
+    const result = simpleCalculator({a: 3, b: 5, action: '^'});
+    expect(result).toBe(243);
   });
 
   test('should return null for invalid action', () => {
-    // Write your test here
+    const result = simpleCalculator({a: 3, b: 5, action: '$'});
+    expect(result).toBeNull();
   });
 
   test('should return null for invalid arguments', () => {
-    // Write your test here
+    const result1 = simpleCalculator({ a: 'NaN', b: 5, action: Action.Add });
+    const result2 = simpleCalculator({ a: 3, b: 'NaN', action: Action.Add });
+    const result3 = simpleCalculator({ a: null, b: 5, action: Action.Add });
+    const result4 = simpleCalculator({ a: 3, b: undefined, action: Action.Add });
+    const result5 = simpleCalculator({ a: false, b: 5, action: Action.Add });
+
+    expect(result1).toBeNull();
+    expect(result2).toBeNull();
+    expect(result3).toBeNull();
+    expect(result4).toBeNull();
+    expect(result5).toBeNull();
   });
 });
